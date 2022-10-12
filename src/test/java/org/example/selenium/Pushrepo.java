6134224c1a43a349c840ae2c2624c91db88de0a9
Sunny4217981 <115615818+Sunny4217981@users.noreply.github.com>
2022-10-12 13:41:39 +0530

    Initial commit

---
+++ src/test/java/org/example/selenium/Pushrepo.java
@@ -0,0 +1,14 @@
+package org.example.selenium;
+
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class Pushrepo {
+
+    @Test
+    public void test1(){
+        System.out.println("Hello");
+    }
+
+}
